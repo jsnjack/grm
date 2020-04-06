c1290cbcb8cee7b65eac8bea5ff3a819ce80738b
Yauhen Shulitski <jsnjack@gmail.com>
2020-04-06 21:15:33 +0200

    :m: Add list command

---
+++ cmd/list.go
@@ -0,0 +1,60 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	bolt "go.etcd.io/bbolt"
+)
+
+const listPattern = "%-20s %s\n"
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List installed packages",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Printf(listPattern, "Package", "Version")
+		err := DB.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(PackagesBucket))
+			c := b.Cursor()
+			for key, _ := c.First(); key != nil; key, _ = c.Next() {
+				pb := b.Bucket(key)
+				if pb != nil {
+					fmt.Printf(listPattern, string(key), string(pb.Get([]byte("version"))))
+				}
+			}
+			return nil
+		})
+		return err
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
