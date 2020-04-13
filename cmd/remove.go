4cbd50af9225c7e0fc0354017b3f7ff64b31905d
Yauhen Shulitski <jsnjack@gmail.com>
2020-04-13 21:20:31 +0200

    :p: Simplify package removal

--- cmd/remove.go
+++ cmd/remove.go
@@ -26,53 +26,41 @@ var removeCmd = &cobra.Command{
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		cmd.SilenceUsage = true
-		pkgs, err := loadInstalledFromDB()
-		if err != nil {
-			return err
-		}
 		for _, item := range args {
-			// Check installed packages
-			for _, p := range pkgs {
-				if item == p.GetFullName() {
-					var location string
-					// Retrieve binary location
-					err = DB.View(func(tx *bolt.Tx) error {
-						b := tx.Bucket([]byte(PackagesBucket))
-						c := b.Cursor()
-						for key, _ := c.First(); key != nil; key, _ = c.Next() {
-							if string(key) == item {
-								pb := b.Bucket(key)
-								if pb != nil {
-									location = string(pb.Get([]byte("filename")))
-								} else {
-									return fmt.Errorf("Bucket %s doesn't exist", item)
-								}
-								return nil
-							}
-
+			var location string
+			// Retrieve binary location
+			err := DB.View(func(tx *bolt.Tx) error {
+				b := tx.Bucket([]byte(PackagesBucket))
+				c := b.Cursor()
+				for key, _ := c.First(); key != nil; key, _ = c.Next() {
+					if string(key) == item {
+						pb := b.Bucket(key)
+						if pb == nil {
+							return fmt.Errorf("Bucket %s doesn't exist", item)
 						}
-						return nil
-					})
-					if err != nil {
-						return err
-					}
-					if ok := askForConfirmation(fmt.Sprintf("Are you sure you want to remove %s?", item)); !ok {
+						location = string(pb.Get([]byte("filename")))
 						return nil
 					}
-					// Remove binary
-					err = removeBinary(location)
-					if err != nil {
-						return err
-					}
-					// Clean db
-					err = DB.Update(func(tx *bolt.Tx) error {
-						bucket := tx.Bucket(PackagesBucket)
-						return bucket.DeleteBucket([]byte(item))
-					})
-					return err
 				}
+				return fmt.Errorf("Package %s is not installed", item)
+			})
+			if err != nil {
+				return err
 			}
-
+			if ok := askForConfirmation(fmt.Sprintf("Are you sure you want to remove %s?", item)); !ok {
+				return nil
+			}
+			// Remove binary
+			err = removeBinary(location)
+			if err != nil {
+				return err
+			}
+			// Clean db
+			err = DB.Update(func(tx *bolt.Tx) error {
+				bucket := tx.Bucket(PackagesBucket)
+				return bucket.DeleteBucket([]byte(item))
+			})
+			return err
 		}
 		return nil
 	},
