ef8e2cff6c5fd4d63c34dbe5930d3663ca19fba1
Yauhen Shulitski <jsnjack@gmail.com>
2020-04-18 19:16:56 +0200

    :p: Fix list layout

--- cmd/list.go
+++ cmd/list.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -18,9 +19,15 @@ var listCmd = &cobra.Command{
 			return err
 		}
 		if len(pkgs) > 0 {
-			fmt.Printf(listPattern, "Package", "Version", "Filter", "Locked")
+			fmt.Printf(listPattern, "Package", "Version", "Locked", "Filter")
 			for _, p := range pkgs {
-				fmt.Printf(listPattern, p.Owner+"/"+p.Repo, p.Version, p.Filter, p.Locked)
+				fmt.Printf(
+					listPattern,
+					p.Owner+"/"+p.Repo,
+					p.Version,
+					p.Locked,
+					fmt.Sprintf(strings.Join(p.Filter, ", "))
+				)
 			}
 		}
 		return nil
