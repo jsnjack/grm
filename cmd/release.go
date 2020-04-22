c12a964dc89fd3d8dda2358ae6565215ff00acb0
Yauhen Shulitski <jsnjack@gmail.com>
2020-04-22 23:29:36 +0200

    :m: Add release command

---
+++ cmd/release.go
@@ -0,0 +1,125 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"mime"
+	"os"
+	"path/filepath"
+
+	"github.com/google/go-github/v30/github"
+	"github.com/schollz/progressbar/v2"
+	"github.com/spf13/cobra"
+	"golang.org/x/oauth2"
+)
+
+var releaseFilename []string
+var releaseTag string
+var releaseGithubToken string
+
+// releaseCmd represents the release command
+var releaseCmd = &cobra.Command{
+	Use:   "release <package>",
+	Short: "Create a release in GitHub",
+	Args: func(cmd *cobra.Command, args []string) error {
+		cmd.SilenceErrors = true
+		switch len(args) {
+		case 0:
+			return fmt.Errorf("requires a package name (e.g. jsnjack/kazy-go)")
+		case 1:
+			_, err := CreatePackage(args[0])
+			if err != nil {
+				return fmt.Errorf("requires a package name (e.g. jsnjack/kazy-go), got %s", args[0])
+			}
+		default:
+			return fmt.Errorf("expected 1 argument, got %d", len(args))
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cmd.SilenceUsage = true
+
+		pkg, err := CreatePackage(args[0])
+		if err != nil {
+			return err
+		}
+
+		// Retrieve GitHub API token
+		if releaseGithubToken == "" {
+			releaseGithubToken = os.Getenv("GITHUB_TOKEN")
+		}
+		if releaseGithubToken == "" {
+			return fmt.Errorf("Provide GitHub API token to create a release")
+		}
+
+		ctx := context.Background()
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: releaseGithubToken},
+		)
+		tc := oauth2.NewClient(ctx, ts)
+
+		client := github.NewClient(tc)
+
+		// Create a release first
+		release, _, err := client.Repositories.CreateRelease(ctx, pkg.Owner, pkg.Repo, &github.RepositoryRelease{
+			TagName: &releaseTag,
+		})
+		if err != nil {
+			return err
+		}
+
+		// Upload assets
+		for _, item := range releaseFilename {
+			fmt.Printf("Uploading %s...\n", item)
+			f, err := os.Open(item)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+
+			stat, err := f.Stat()
+			if err != nil {
+				return err
+			}
+
+			if stat.IsDir() {
+				return fmt.Errorf("The asset to upload can't be a directory")
+			}
+
+			bar := progressbar.NewOptions(
+				int(stat.Size()),
+				progressbar.OptionSetBytes(int(stat.Size())),
+			)
+
+			u := fmt.Sprintf("repos/%s/%s/releases/%d/assets?name=%s", pkg.Owner, pkg.Repo, release.GetID(), filepath.Base(item))
+
+			mediaType := mime.TypeByExtension(filepath.Ext(f.Name()))
+
+			req, err := client.NewUploadRequest(u, bar, stat.Size(), mediaType)
+			if err != nil {
+				return err
+			}
+
+			asset := new(github.ReleaseAsset)
+			_, err = client.Do(ctx, req, asset)
+			if err != nil {
+				return err
+			}
+			fmt.Println("")
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(releaseCmd)
+	releaseCmd.Flags().StringArrayVarP(&releaseFilename, "filename", "f", releaseFilename, "Location of an asset to upload")
+	releaseCmd.MarkFlagFilename("filename")
+	releaseCmd.MarkFlagRequired("filename")
+
+	releaseCmd.Flags().StringVarP(&releaseTag, "tag", "t", "", "Tag name")
+	releaseCmd.MarkFlagRequired("tag")
+
+	releaseCmd.Flags().StringVarP(&releaseGithubToken, "token", "g", "", "GitHub API token")
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module github.com/jsnjack/grm
 go 1.13
 
 require (
+	github.com/golang/protobuf v1.4.0 // indirect
 	github.com/google/go-github/v30 v30.1.0
 	github.com/mholt/archiver/v3 v3.3.0
 	github.com/schollz/progressbar/v2 v2.15.0
 	github.com/spf13/cobra v0.0.7
 	go.etcd.io/bbolt v1.3.4
 	golang.org/x/crypto v0.0.0-20200323165209-0ec3e9974c59 // indirect
+	golang.org/x/net v0.0.0-20200421231249-e086a090c8fd // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	google.golang.org/appengine v1.6.6 // indirect
 )
