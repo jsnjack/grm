a755d63745d2349501dec73d625801b0424f300e
Yauhen Shulitski <jsnjack@gmail.com>
2020-11-15 22:19:37 +0100

    :m: Print package description from github

--- cmd/list.go
+++ cmd/list.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"strings"
 
@@ -8,6 +9,9 @@ import (
 )
 
 const listPattern = "%-40s %-20s %-20s %s\n"
+const listRepoDescriptionPattern = "%-40s %s\n"
+
+var listRepoDescription bool
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
@@ -19,15 +23,35 @@ var listCmd = &cobra.Command{
 			return err
 		}
 		if len(pkgs) > 0 {
-			fmt.Printf(listPattern, "Package", "Version", "Locked", "Filter")
-			for _, p := range pkgs {
-				fmt.Printf(
-					listPattern,
-					p.Owner+"/"+p.Repo,
-					p.Version,
-					p.GetVerboseLocked(),
-					fmt.Sprintf(strings.Join(p.Filter, ", ")),
-				)
+			if listRepoDescription {
+				// Print list of all packages and fetch their description from github
+				fmt.Printf(listRepoDescriptionPattern, "Package", "Description")
+				client := CreateClient()
+				for _, p := range pkgs {
+					var description string
+					repo, _, err := client.Repositories.Get(context.Background(), p.Owner, p.Repo)
+					if err != nil {
+						description = err.Error()
+					} else {
+						description = repo.GetDescription()
+					}
+					fmt.Printf(
+						listRepoDescriptionPattern,
+						p.GetFullName(),
+						description,
+					)
+				}
+			} else {
+				fmt.Printf(listPattern, "Package", "Version", "Locked", "Filter")
+				for _, p := range pkgs {
+					fmt.Printf(
+						listPattern,
+						p.GetFullName(),
+						p.Version,
+						p.GetVerboseLocked(),
+						fmt.Sprintf(strings.Join(p.Filter, ", ")),
+					)
+				}
 			}
 		}
 		return nil
@@ -35,5 +59,6 @@ var listCmd = &cobra.Command{
 }
 
 func init() {
+	listCmd.Flags().BoolVarP(&listRepoDescription, "description", "d", false, "Print repository description")
 	rootCmd.AddCommand(listCmd)
 }
